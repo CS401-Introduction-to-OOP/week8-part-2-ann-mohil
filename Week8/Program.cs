4322355369982c7499f529bb99f8651132078f68
ann-mohil <anna.mohil.25@kse.org.ua>
2026-03-28 17:31:31 +0200

    last stage

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,9 +1,25 @@
 ﻿
+using Week8;
+
 class Program
 {
     public static void Main()
     {
+        var file = new FileResource("report.txt");
+        var network = new NetworkResource("company.local");
+        var manager = new ResourceManager<Resource>();
         
+        manager.Add(file);
+        manager.Add(network);
+        manager.OpenAll();
+
+        using (var res = new FileResource("temporary.txt"))
+        {
+            res.Open();
+        }
+        
+        manager.CloseAll();
+        Console.WriteLine($"Done.");
     }
     
 }
